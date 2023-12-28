246f087b8fe83a5ffcd6b37fdb330e14cb70d0da
Luan Matos <72422359+matosluan@users.noreply.github.com>
2023-12-28 10:18:07 -0300

    Update db.dart

--- lib/db.dart
+++ lib/db.dart
@@ -1,11 +1,11 @@
 import 'package:mysql1/mysql1.dart';
 
 class Mysql {
-  static String host = '***.***.**.**',
-      user = 'lab',
-      password = 'lab',
-      db = 'lab';
-  static int port = 0000;
+  static String host = '',
+      user = '',
+      password = '',
+      db = '';
+  static int port = ;
 
   Mysql();
 
